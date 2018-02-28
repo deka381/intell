24e6e60447afe41b07c17c13587d6a8607f2952c
Konrad <konrad381@gmail.com>
2018-02-28 15:29:47 +0100

    part3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 // slider Pakiety pobytowe START
-let responsiveSlider = function() {
+let section2Slider = function() {
   let slider = document.querySelector(".carusel");
   let sliderWidth = slider.offsetWidth;
   console.log(sliderWidth);
@@ -56,10 +56,85 @@ next.addEventListener("click", function() {
 
 };
 window.onload = function() {
-  responsiveSlider();  
+  section2Slider();  
 }
 // slider Pakiety pobytowe END
 
+// slider Galery START
+let GalerySlider = function(x,pause,play) {
+  let sliderGalery = document.querySelector(".slide_section"+x);
+  let sliderWidthGalery = sliderGalery.offsetWidth;
+  let sliderListGalery = document.querySelector(".ul_section"+x);
+  let countGalery =1;
+  let itemsGalery = sliderListGalery.querySelectorAll(".img_slide_section"+x).length;
+  let prevGalery = document.querySelector(".slider"+x+"-prev");
+  let nextGalery = document.querySelector(".slider"+x+"-next");
+  let pauseGalery = document.querySelector(".slider"+x+"-pause");
 
+  window.addEventListener('resize', function() {
+    sliderWidthGalery = sliderGalery.offsetWidth;
+  });
+
+  var prevSlideGalery = function() {
+    if(countGalery > 1) {
+      countGalery = countGalery - 2;
+      sliderListGalery.style.left = "-" + countGalery * sliderWidthGalery + "px";
+      countGalery++;
+    }
+    else if(countGalery = 1) {
+      countGalery = itemsGalery - 1;
+      sliderListGalery.style.left = "-" + countGalery * sliderWidthGalery + "px";
+      countGalery++;
+    }
+  };
+
+  var nextSlideGalery = function() {
+    if(countGalery < itemsGalery) {
+      sliderListGalery.style.left = "-" + countGalery * sliderWidthGalery + "px";
+      countGalery++;
+    }
+    else if(countGalery = itemsGalery) {
+      sliderListGalery.style.left = "0px";
+      countGalery = 1;
+    }
+  };
+
+nextGalery.addEventListener("click", function() {
+    nextSlideGalery();
+  });
+
+  prevGalery.addEventListener("click", function() {
+    prevSlideGalery();
+  });
+
+    let click = 2;
+    var startStop;
+    
+
+  pauseGalery.addEventListener("click", function(){
+    if (click === 2) {
+      nextSlideGalery();
+    }
+    if (click%2===0){
+      startStop= setInterval(function() {
+      nextSlideGalery()
+      }, 5000);
+      pauseGalery.style.backgroundImage = "url('img/svg/"+pause+"')";
+    }else{
+      clearInterval(startStop);
+      pauseGalery.style.backgroundImage = "url('img/svg/"+play+"')";
+      }
+
+      click++;
+    
+  });
+      
+
+};
+window.onload = function() {
+  GalerySlider(4,"pause.png","play.svg");  
+  GalerySlider(6,"pauseWhite.png","playWhite.png"); 
+}
+// slider Galery END
 
 });
